71d535428b708a52eaa065ca47b675cde49994df
202006359 <202006359@alu.comillas.edu>
2023-05-12 17:10:27 +0200

    Subo proyecto basico

---
+++ src/main/java/edu/comillas/icai/pat/ejemplopat/service/MyService.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Universidad de Comillas ICAI.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the company
+ * which accompanies this distribution
+ *
+ * Contributors:
+ *     Universidad de Comillas - ICAI
+ *******************************************************************************/
+
+
+package edu.comillas.icai.pat.ejemplopat.service;
+
+import edu.comillas.icai.pat.ejemplopat.dto.Contact;
+
+public interface MyService {
+
+    String getHealthCheck(String name);
+    Contact getDemoUser(Integer id);
+
+
+}
