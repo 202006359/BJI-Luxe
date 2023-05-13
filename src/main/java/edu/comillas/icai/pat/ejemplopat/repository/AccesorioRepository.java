99fafd40917f76a1111804f517513fcd60c7b88d
Javier <javier.del.aguila@hotmail.com>
2023-05-13 17:42:03 +0200

    ID ahora es Long

--- src/main/java/edu/comillas/icai/pat/ejemplopat/repository/AccesorioRepository.java
+++ src/main/java/edu/comillas/icai/pat/ejemplopat/repository/AccesorioRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 import edu.comillas.icai.pat.ejemplopat.model.AccesorioModel;
 
-public interface AccesorioRepository extends CrudRepository<Integer, AccesorioModel>
+public interface AccesorioRepository extends CrudRepository<AccesorioModel,Long>
 {
 
     @Query
@@ -19,13 +19,13 @@ public interface AccesorioRepository extends CrudRepository<Integer, AccesorioMo
 
     @Query
     ("SELECT * FROM ACCESORIOS WHERE ID= :searched_id")
-    public AccesorioModel getById(@Param ("searched_id") int id);
+    public AccesorioModel getById(@Param ("searched_id") Long id);
 
     @Modifying
     @Query
     ("INSERT INTO ACCESORIOS (ID, NOMBRE, DIR_IMAGE, PRECIO) VALUES(:id, :nombre, :dir_image, :precio)")
     public void createAccesorio(
-        @Param("id") int id,
+        @Param("id") Long id,
         @Param("nombre") String nombre,
         @Param("dir_image") String dir_image,
         @Param("Precio") float precio
@@ -37,7 +37,7 @@ public interface AccesorioRepository extends CrudRepository<Integer, AccesorioMo
     ("UPDATE ACCESORIOS SET PRECIO=:precio WHERE ID=:id")
     public void updatePrecio(
         @Param("precio") float precio,
-        @Param("id") int id
+        @Param("id") Long id
     );
 
 
