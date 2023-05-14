43e8402404510cf321e47139593123ceaa0e0ae9
beaorbe <67244352+beaorbe@users.noreply.github.com>
2023-05-14 15:45:50 +0200

    COMMIT

--- src/main/resources/static/mujeres.js
+++ src/main/resources/static/mujeres.js
@@ -16,28 +16,28 @@ window.onload = function getProductos()
     let html1="";
     html1 += `<img src="${data[0].url}" alt="Mujer 3" height="300em" >`+
              `<h3>${data[0].nombre}</h3>`+
-             `<p>$${data[0].precio}</p>` 
+             `<h3>${data[0].precio} EUR</h3>` 
    
     document.getElementById("prenda1").innerHTML = html1;
 
     let html2="";
     html2 += `<img src="${data[1].url}" alt="Mujer 3" height="300em" >`+
              `<h3>${data[1].nombre}</h3>`+
-             `<p>$${data[1].precio}</p>`
+             `<h3>${data[1].precio} EUR</h3>`
     document.getElementById("prenda2").innerHTML = html2;
 
 
     let html3="";
     html3 += `<img src="${data[2].url}" alt="Mujer 3" height="300em" >`+
              `<h3>${data[2].nombre}</h3>`+
-             `<p>$${data[2].precio}</p>`
+             `<h3>${data[2].precio} EUR</h3>`
     document.getElementById("prenda3").innerHTML = html3;
-
+ 
   
     let html4="";
     html4 += `<img src="${data[3].url}" alt="Mujer 3" height="300em" >`+
              `<h3>${data[3].nombre}</h3>`+
-             `<p>$${data[3].precio}</p>`
+             `<h3>$${data[3].precio} EUR</h3>`
 
     document.getElementById("prenda4").innerHTML = html4;
 
@@ -45,13 +45,13 @@ window.onload = function getProductos()
     let html5="";
     html5 += `<img src="${data[4].url}" alt="Mujer 3" height="300em" >`+
              `<h3>${data[4].nombre}</h3>`+
-             `<p>$${data[4].precio}</p>`
+             `<h3>${data[4].precio} EUR</h3>`
     document.getElementById("prenda5").innerHTML = html5;
 
     let html6="";
     html6 += `<img src="${data[5].url}" alt="Mujer 3" height="300em" >`+
              `<h3>${data[5].nombre}</h3>`+
-             `<p>$${data[5].precio}</p>`
+             `<h3>${data[5].precio} EUR</h3>`
     document.getElementById("prenda6").innerHTML = html6;
     datos_stock.push(data);
     //console.log(datos);
